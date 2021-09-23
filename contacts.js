405007dc8179ce5fc5d5b271672cdb07ecbda9ea
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-09-23 23:02:34 +0300

    finally

---
+++ contacts.js
@@ -0,0 +1,57 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve("db/contacts.json");
+
+async function findContactInArrById(arr, id) {
+  return await arr.find((el) => el.id.toString() === id.toString());
+}
+
+async function listContacts() {
+  try {
+    return await fs.readFile(contactsPath).then(JSON.parse);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return await findContactInArrById(contacts, contactId);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    if (!contactId) throw new Error("Unknown ID");
+
+    const contacts = await listContacts();
+    const newContacts = JSON.stringify(
+      contacts.filter((el) => el.id.toString() !== contactId.toString())
+    );
+    fs.writeFile(contactsPath, newContacts);
+    return findContactInArrById(contacts, contactId);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    if (!name || !email || !phone) throw new Error("Unknown data");
+
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    const newContacts = JSON.stringify([...contacts, newContact]);
+    fs.writeFile(contactsPath, newContacts);
+    return newContact;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
